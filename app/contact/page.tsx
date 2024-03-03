d553f4569bb7d1195d316f05eeccaf79c27fd6f7
Roberto <rob.riveros.es@gmail.com>
2024-03-03 22:32:31 +0100

    add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import ContactPage from "@/components/ContactPage";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <div className="h-16"></div>
+      <ContactPage />
+    </div>
+  );
+};
+
+export default Page;
